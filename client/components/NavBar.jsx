f29e1c4dbcc075827bc280eda4ba8d5a4edf3183
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2024-08-10 23:49:49 +0530

    update login button

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -117,7 +117,7 @@ function NavBar() {
           </div>
 
           <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">
+            <div className="hidden sm:flex sm:gap-4">
               <a
                 className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
                 href="#"
