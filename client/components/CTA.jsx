1705246d13634c28318eba35e676f6961f741f43
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2024-08-11 11:06:43 +0530

    add clsx effect

--- client/components/CTA.jsx
+++ client/components/CTA.jsx
@@ -1,52 +1,56 @@
+"use client";
 import React from "react";
+import LazyLoadSection from "@/components/LazyLoadingSection";
 
 function CTA() {
   return (
-    <section className="">
-      <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
-          <div className="relative z-10 lg:py-16">
-            <div className="relative h-64 sm:h-80 lg:h-full">
-              <img
-                alt=""
-                src="/assets/cta.jpg"
-                className="absolute inset-0 h-full w-full object-cover rounded-3xl"
-              />
+    <LazyLoadSection>
+      <section className="">
+        <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
+            <div className="relative z-10 lg:py-16">
+              <div className="relative h-64 sm:h-80 lg:h-full">
+                <img
+                  alt=""
+                  src="/assets/cta.jpg"
+                  className="absolute inset-0 h-full w-full object-cover rounded-3xl"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="relative flex items-center bg-gray-100">
-            <span className="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-gray-100"></span>
+            <div className="relative flex items-center bg-gray-100">
+              <span className="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-gray-100"></span>
 
-            <div className="p-8 sm:p-16 lg:p-24">
-              <h2 className="text-2xl font-bold sm:text-3xl">
-                A little about Tea Jar and the KDU Group
-              </h2>
+              <div className="p-8 sm:p-16 lg:p-24">
+                <h2 className="text-2xl font-bold sm:text-3xl">
+                  A little about Tea Jar and the KDU Group
+                </h2>
 
-              <p className="mt-4 text-gray-600">
-                We are perceived as one of the pre-eminent and largest tea
-                manufacturers in Sri Lanka, provisioning a 4 percent of the
-                entire tea production in the country. Under the brand name “Tea
-                Jar” at K.D.U. Exports, we produce 11 million teas consisting 6
-                percent of the Low Grown category, within Sri lanka. Going from
-                strength to strength, sustaining from the K.D.U GROUP, the
-                synthesis of ingrained commitment, dedication and diligence has
-                put ourselves at the summit of the tea industry. We are a great
-                equalizer, a demanding attention from the counter of the
-                smallest eatery to the most exclusive tea-bars in the world.
-                Proudly claiming for a history of more than three decades of tea
-                production, teas from Tea Jar possesses unique and specific
-                characteristics of quality and taste attributed by the Passion
-                and Provenance of the unique manufacturing practices, that have
-                been mastered over the years, infused with contemporary
-                creativity and imagination to satisfy the most judicious
-                aficionado.
-              </p>
+                <p className="mt-4 text-gray-600">
+                  We are perceived as one of the pre-eminent and largest tea
+                  manufacturers in Sri Lanka, provisioning a 4 percent of the
+                  entire tea production in the country. Under the brand name
+                  “Tea Jar” at K.D.U. Exports, we produce 11 million teas
+                  consisting 6 percent of the Low Grown category, within Sri
+                  lanka. Going from strength to strength, sustaining from the
+                  K.D.U GROUP, the synthesis of ingrained commitment, dedication
+                  and diligence has put ourselves at the summit of the tea
+                  industry. We are a great equalizer, a demanding attention from
+                  the counter of the smallest eatery to the most exclusive
+                  tea-bars in the world. Proudly claiming for a history of more
+                  than three decades of tea production, teas from Tea Jar
+                  possesses unique and specific characteristics of quality and
+                  taste attributed by the Passion and Provenance of the unique
+                  manufacturing practices, that have been mastered over the
+                  years, infused with contemporary creativity and imagination to
+                  satisfy the most judicious aficionado.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </LazyLoadSection>
   );
 }
 
--- client/components/TopSellers.jsx
+++ client/components/TopSellers.jsx
@@ -15,6 +15,7 @@ import {
 } from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import LazyLoadSection from "@/components/LazyLoadingSection";
 
 // Import Swiper styles
 import "swiper/css";
@@ -45,97 +46,99 @@ function TopSellers() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <section
-      ref={sectionRef}
-      style={{ backgroundColor: bgColor }}
-      className="transition-all duration-500 lg:min-h-screen lg:flex lg:items-center overflow-hidden"
-    >
-      <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
-        <SectionHeader
-          sectionTitle="Top Sellers"
-          sectionHighlight="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum similique quos fugit nobis labore est voluptate in aliquam voluptates quod nulla, odio repudiandae fugiat, alias eos consectetur, repellendus exercitationem earum!"
-        />
+    <LazyLoadSection>
+      <section
+        ref={sectionRef}
+        style={{ backgroundColor: bgColor }}
+        className="transition-all duration-500 lg:min-h-screen lg:flex lg:items-center overflow-hidden"
+      >
+        <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
+          <SectionHeader
+            sectionTitle="Top Sellers"
+            sectionHighlight="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum similique quos fugit nobis labore est voluptate in aliquam voluptates quod nulla, odio repudiandae fugiat, alias eos consectetur, repellendus exercitationem earum!"
+          />
 
-        <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={2}
-          initialSlide={1}
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 3,
-            slideShadows: true,
-          }}
-          keyboard={{
-            enabled: true,
-          }}
-          mousewheel={{
-            thresholdDelta: 70,
-          }}
-          modules={[EffectCoverflow, Navigation]}
-          className="my-14"
-          breakpoints={{
-            640: {
-              slidesPerView: 2,
-            },
-            768: {
-              slidesPerView: 2,
-            },
-            1024: {
-              slidesPerView: 3,
-            },
-          }}
-          pagination={{
-            dynamicBullets: true,
-          }}
-        >
-          <SwiperSlide>
-            <TopSellerProduct
-              imgUrl="/assets/products/1/apple.jpg"
-              productName="Apple Flavored Tea Bags"
-              range="Flavoured"
-              miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
-            />
-          </SwiperSlide>
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={2}
+            initialSlide={2}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 100,
+              modifier: 3,
+              slideShadows: true,
+            }}
+            keyboard={{
+              enabled: true,
+            }}
+            mousewheel={{
+              thresholdDelta: 70,
+            }}
+            modules={[EffectCoverflow, Navigation]}
+            className="my-14"
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+            }}
+            pagination={{
+              dynamicBullets: true,
+            }}
+          >
+            <SwiperSlide>
+              <TopSellerProduct
+                imgUrl="/assets/products/1/apple.jpg"
+                productName="Apple Flavored Tea Bags"
+                range="Flavoured"
+                miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
+              />
+            </SwiperSlide>
 
-          <SwiperSlide>
-            <TopSellerProduct
-              imgUrl="/assets/products/1/cardamom.jpg"
-              productName="Cardamom Flavored Tea Bags"
-              range="Flavoured"
-              miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <TopSellerProduct
-              imgUrl="/assets/products/1/cinnamon.jpg"
-              productName="Cinnamon Flavored Tea Bags"
-              range="Flavoured"
-              miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <TopSellerProduct
-              imgUrl="/assets/products/1/ginger.jpg"
-              productName="Ginger Flavored Tea Bags"
-              range="Flavoured"
-              miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <TopSellerProduct
-              imgUrl="/assets/products/1/ginger.jpg"
-              productName="Ginger Flavored Tea Bags"
-              range="Flavoured"
-              miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
-            />
-          </SwiperSlide>
-        </Swiper>
-      </div>
-    </section>
+            <SwiperSlide>
+              <TopSellerProduct
+                imgUrl="/assets/products/1/cardamom.jpg"
+                productName="Cardamom Flavored Tea Bags"
+                range="Flavoured"
+                miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <TopSellerProduct
+                imgUrl="/assets/products/1/cinnamon.jpg"
+                productName="Cinnamon Flavored Tea Bags"
+                range="Flavoured"
+                miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <TopSellerProduct
+                imgUrl="/assets/products/1/ginger.jpg"
+                productName="Ginger Flavored Tea Bags"
+                range="Flavoured"
+                miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <TopSellerProduct
+                imgUrl="/assets/products/1/ginger.jpg"
+                productName="Ginger Flavored Tea Bags"
+                range="Flavoured"
+                miniDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis, aperiam nisi omnis voluptas iusto repudiandae optio atque neque cupiditate doloribus at mollitia animi ad a sint, quae magnam laboriosam?"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </section>
+    </LazyLoadSection>
   );
 }
 
