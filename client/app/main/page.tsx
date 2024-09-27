cf3e1d6e989a12236fbfaf5827b920d6c14c36da
yomal321 <yomaltheekshan9988@gmail.com>
2024-09-27 13:19:12 +0530

    fix sum problem

--- client/app/main/page.tsx
+++ client/app/main/page.tsx
@@ -3,7 +3,7 @@ import Shop from "@/components/Shop";
 
 function page() {
   return (
-    <div className="container px-4 sm:px-6 md:px-12 lg:px-16 xl:px-24 my-5 md:my-20">
+    <div className="container px-4 sm:px-6 md:px-8 lg:px-10 xl:px-16 my-5 md:my-20">
       <Shop />
     </div>
   );
