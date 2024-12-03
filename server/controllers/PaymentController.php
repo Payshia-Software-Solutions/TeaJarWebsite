345ec06a2284f62a652168e2a0c44729ad2e4519
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2024-12-03 13:57:06 +0530

    Update PaymentController.php

--- server/controllers/PaymentController.php
+++ server/controllers/PaymentController.php
@@ -348,7 +348,7 @@ class PaymentController
 
             // Generate the HTML form that will auto-submit to PayHere checkout
             echo '<html><body onload="document.forms[0].submit();">';
-            echo '<form method="post" action="https://sandbox.payhere.lk/pay/checkout">';
+            echo '<form method="post" action="https://live.payhere.lk/pay/checkout">';
             foreach ($form_data as $key => $value) {
                 echo '<input type="hidden" name="' . htmlspecialchars($key) . '" value="' . htmlspecialchars($value) . '">';
             }
