f547f0c75d07d621dcb23e438a705f38f3522506
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2025-11-27 18:02:45 +0530

    feat: add sitemap, winter promo banner, and discount modal component.

--- client/components/Common/DiscountModel.jsx
+++ client/components/Common/DiscountModel.jsx
@@ -69,7 +69,7 @@ const DiscountModel = () => {
           {/* Left side content */}
           <div className=" text-white rounded-t-lg relative w-1/2">
             <Image
-              src="/assets/promo-banners/august-banner.webp"
+              src="/assets/promo-banners/winter-banner-optimized.webp"
               alt="Promo Banner"
               className="rounded-l-lg object-fill"
               layout="intrinsic" // Maintain aspect ratio
